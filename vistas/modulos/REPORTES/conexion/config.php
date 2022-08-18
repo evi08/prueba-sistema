3cde2bcd7d45856503d1e91286b4c089c71cf18a
Alex Elan Sánches Vilches <102844702+evi08@users.noreply.github.com>
2022-08-18 17:17:49 -0600

    Delete vistas directory

--- vistas/modulos/REPORTES/conexion/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-    define('DB_HOST','nph.ca7eckccl7rg.us-east-2.rds.amazonaws.com:3306');
-    define('DB_NAME','nph');
-    define('DB_USER','admin');
-    define('DB_PASSWORD','Sistemas.2020');
-    define('DB_CHARSET','utf8');
-
-?>
